9b4b7ed6b0abe43ef9d193056a039d387726a447
Lucas Palma <lucaspalma44@gmail.com>
2022-02-02 00:57:22 -0300

    Initial commit

---
+++ Aula02/src/aula02/Caneta.java
@@ -0,0 +1,30 @@
+package aula02;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() {
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("está tampada? " + this.tampada);
+        System.out.println("qual a ponta dela? " + this.ponta);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Carga: " + this.carga);
+    }
+    void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou Rabiscando");
+        }
+    }
+    
+    void tampar() {
+        this.tampada = true;
+    }
+    
+    void destampar() {
+        this.tampada = false;
+    }
+}
